6e945c5904df7ac82b76fca90d8e16bb7d94c510
johannes117 <51395795+johannes117@users.noreply.github.com>
2020-10-22 13:08:24 +1100

    Added Heart Rate Tracker View Controller, Also re-added HealthKitManager with no errors. Both Watch and iPhone app share healthKitManager()

---
+++ Shared/HealthKitManager.swift
@@ -0,0 +1,69 @@
+//
+//  HealthKitManager.swift
+//  AerabyteFitnessTracker
+//
+//  Created by Johannes du Plessis on 22/10/20.
+//
+
+import Foundation
+import HealthKit
+
+protocol HeartRateDelegate {
+    func heartRateUpdated(heartRateSamples: [HKSample])
+}
+
+class HealthKitManager: NSObject {
+    static let sharedInstance = HealthKitManager()
+    
+    private override init() {}
+    
+    let healthStore = HKHealthStore()
+    
+    var anchor: HKQueryAnchor?
+    
+    var heartRateDelegate: HeartRateDelegate?
+    
+    func authorizeHealthKit(_ completion: @escaping ((_ success: Bool, _ error: Error?) -> Void)) {
+        
+        guard let heartRateType: HKQuantityType = HKQuantityType.quantityType(forIdentifier: .heartRate) else {
+            return
+        }
+        let typesToShare = Set([HKObjectType.workoutType(), heartRateType])
+        let typesToRead = Set([HKObjectType.workoutType(), heartRateType, HKObjectType.activitySummaryType()])
+        
+        healthStore.requestAuthorization(toShare: typesToShare, read: typesToRead) { (success, error) in
+            print("Was authorization successful? \(success)")
+            completion(success, error)
+        }
+    }
+    
+    func createHeartRateStreamingQuery(_ workoutStartDate: Date) -> HKQuery? {
+        
+        guard let heartRateType: HKQuantityType = HKQuantityType.quantityType(forIdentifier: .heartRate) else {
+            return nil
+        }
+
+        let datePredicate = HKQuery.predicateForSamples(withStart: workoutStartDate, end: nil, options: .strictEndDate)
+        let compoundPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [datePredicate])
+        
+        let heartRateQuery = HKAnchoredObjectQuery(type: heartRateType, predicate: compoundPredicate, anchor: nil, limit: Int(HKObjectQueryNoLimit)) { (query, sampleObjects, deletedObjects, newAnchor, error) in
+            
+            guard let newAnchor = newAnchor,
+                let sampleObjects = sampleObjects else {
+                    return
+            }
+            self.anchor = newAnchor
+            self.heartRateDelegate?.heartRateUpdated(heartRateSamples: sampleObjects)
+        }
+        heartRateQuery.updateHandler = {(query, sampleObjects, deletedObjects, newAnchor, error) -> Void in
+
+            guard let newAnchor = newAnchor,
+                let sampleObjects = sampleObjects else {
+                    return
+            }
+            self.anchor = newAnchor
+            self.heartRateDelegate?.heartRateUpdated(heartRateSamples: sampleObjects)
+        }
+        return heartRateQuery
+    }
+}
